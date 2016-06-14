d026b0f7fb341525c3021daa6b450fc545036464
oraclejet <oraclejet_us@oracle.com>
2016-06-14 15:31:49 -0700

    Pre-release 2.0.2-b1 update 2

--- dist/js/libs/oj/debug/ojcore.js
+++ dist/js/libs/oj/debug/ojcore.js
@@ -46,8 +46,8 @@ var _oldVal = _scope['oj'];
 var oj = _scope['oj'] =
 {
   'version': "2.0.2",
-  'build' : "294",
-  'revision': "26305",
+  'build' : "295",
+  'revision': "26311",
           
   // This function is only meant to be used outside the library, so quoting the name
   // to avoid renaming is appropriate
